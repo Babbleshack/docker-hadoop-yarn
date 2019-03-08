028f554a61f6bbfc9bc7c833d068d1a1aea6b3c6
babbleshack <babbleshack@babblebase.net>
2019-03-08 17:03:57 +0000

    hadoop 2.9.2

--- datanode/files/init_scripts/start_datanode.sh
+++ datanode/files/init_scripts/start_datanode.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+
+rm -rf $HADOOP_DATA_NODE_DATA/*
+rm -rf $HADOOP_NAME_NODE_DATA/*
+rm -rf $HADOOP_TEMPDIR/*
 /etc/init.d/ssh start
-#$HADOOP_HOME/bin/hdfs datanode
-$HADOOP_HOME/bin/hdfs --daemon start datanode
+$HADOOP_HOME/bin/hdfs datanode
+#$HADOOP_HOME/bin/hdfs --daemon start datanode
 #$HADOOP_HOME/sbin/hadoop-daemon.sh start datanode
-tail -f $HADOOP_HOME/logs/hadoop-*-datanode-*.log
+#tail -f $HADOOP_HOME/logs/hadoop-*-datanode-*.log
 #update to datanode logs
 #tail -f /dev/null 
 
--- namenode/files/start_namenode.sh
+++ namenode/files/start_namenode.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
-
+#cat /etc/hosts
+#echo $HOSTNAME
+hostname
+rm -rf $HADOOP_DATA_NODE_DATA/*
+rm -rf $HADOOP_NAME_NODE_DATA/*
+rm -rf $HADOOP_TEMPDIR/*
 /etc/init.d/ssh start
 $HADOOP_HOME/bin/hdfs namenode -format
 $HADOOP_HOME/bin/hdfs namenode 
-
+tail -f $HADDOP_HOME/logs/hadoop-*-namenode-*.log 
