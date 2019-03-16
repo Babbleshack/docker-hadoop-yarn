0dfdef3b502296722dc99e99826b392eee36868f
babbleshack <dcrl94@gmail.com>
2019-03-16 22:46:41 +0000

    HDFS master and Worker

---
+++ base/files/init_scripts/start_worker.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+rm -rf $HADOOP_HOME/hdfs/namenode/*
+rm -rf $HADOOP_HOME/hdfs/datanode/*
+rm -rf $HADOOP_HOME/hadooptmpdata/*
+/etc/init.d/ssh start
+while true; do sleep 10000; done
