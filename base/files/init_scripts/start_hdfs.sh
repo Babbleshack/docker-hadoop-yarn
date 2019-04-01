de42324244db4404209cecde5abb799118e44b0d
babbleshack <dcrl94@gmail.com>
2019-04-01 17:19:30 +0100

    add additional init scripts

---
+++ base/files/init_scripts/start_hdfs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+rm -rf $HADOOP_HOME/hdfs/namenode/*
+rm -rf $HADOOP_HOME/hdfs/datanode/*
+rm -rf $HADOOP_HOME/hadooptmpdata/*
+/etc/init.d/ssh start
+$HADOOP_HOME/bin/hdfs namenode -format
+$HADOOP_HOME/sbin/start-dfs.sh
+while true
+do
+	tail -f $HADOOP_HOME/logs/*.log
+  sleep 5
+done
